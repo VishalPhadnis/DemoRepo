5aa45d55d76e4878955723c76edc5fc8186930e3
Vishal Phadnis <25530@ITCINFOTECH.COM>
2017-11-20 19:17:27 +0530

    second edited git cmmmit for my selenium code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,16 @@
 package com.qa.pages;
 
 public class LoginPage {
+	
+	public void loginpage(){
+		
+		System.out.println("test login");
+	}
+	
+	public void loginpage_1(){
+		
+		System.out.println("test login");
+	}
+
 
 }
